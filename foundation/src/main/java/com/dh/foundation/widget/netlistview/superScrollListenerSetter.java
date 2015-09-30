5d33db4a5b63a3fdc7e126e42c599a1c7d9e6ccf
wuseal <sealkingking@163.com>
2015-09-30 14:01:41 +0800

    １增加NetListView控件适配全数据类型，在HttpNetUtils可用前提下 ２,添加NetListView使用说明 ３．添加NetListView使用Demo

---
+++ foundation/src/main/java/com/dh/foundation/widget/netlistview/superScrollListenerSetter.java
@@ -0,0 +1,20 @@
+package com.dh.foundation.widget.netlistview;
+
+import android.widget.AbsListView;
+
+/**
+ * 父类滚动监听设置器
+ * Created By: Seal.Wu
+ * Date: 2015/9/28
+ * Time: 17:16
+ */
+interface SuperScrollListenerSetter {
+
+
+    /**
+     * 设置滚动监听器
+     *
+     * @param l 监听器
+     */
+    void setSuperOnScrollListener(AbsListView.OnScrollListener l);
+}
